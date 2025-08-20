26b15b828833f550bc9e44a9a75618092f2dbf06
Riccardo Giordanella <it.gg@hotmail.it>
2025-08-20 16:00:57 +0200

    added swiper+modal and fixed css

--- src/Components/MyWork.jsx
+++ src/Components/MyWork.jsx
@@ -3,37 +3,88 @@ import mywork_data from "../assets/mywork_data";
 import white_arrow from "../assets/white_arrow.png";
 import { useState } from "react";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+
 export default function MyWork() {
   const [isHovered, setIsHovered] = useState(false);
+  const [modalImage, setModalImage] = useState(null);
 
   return (
     <section id="projects" className="mywork">
       <div className="mywork-title">
         <h2>My latest work</h2>
       </div>
+
       <div className="mywork-container">
-        {mywork_data.map((work, index) => {
-          return (
-            <div key={index} className="box">
-              <img src={work.w_img} alt="Work image" />
-              <div className="titicon">
-                <h2>{work.w_name}</h2>
-                {work.github && (
-                  <a
-                    href={work.github}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <div className="circle" title="GitHub">
-                      <i className="fa-brands fa-github fa-lg"></i>
-                    </div>
-                  </a>
-                )}
-              </div>
+        {mywork_data.map((work, index) => (
+          <div key={index} className="box">
+            {work.w_img.length > 1 ? (
+              <Swiper
+                modules={[Navigation]}
+                navigation
+                spaceBetween={10}
+                slidesPerView={1}
+                className="mywork-swiper"
+              >
+                {work.w_img.map((img, imgIndex) => (
+                  <SwiperSlide key={imgIndex}>
+                    <img
+                      src={img}
+                      alt={`Project ${index + 1} img ${imgIndex + 1}`}
+                      onClick={() => setModalImage(img)}
+                    />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            ) : (
+              <img
+                src={work.w_img[0]}
+                alt={`Project ${index + 1} image`}
+                onClick={() => setModalImage(work.w_img[0])}
+              />
+            )}
+
+            <div className="titicon">
+              <h2>{work.w_name || "Untitled Project"}</h2>
+              {work.github && (
+                <a
+                  href={work.github}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <div className="circle" title="GitHub">
+                    <i className="fa-brands fa-github fa-lg"></i>
+                  </div>
+                </a>
+              )}
             </div>
-          );
-        })}
+          </div>
+        ))}
       </div>
+
+      {modalImage && (
+        <div
+          className="custom-modal-overlay"
+          onClick={() => setModalImage(null)}
+        >
+          <div
+            className="custom-modal-content"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <button
+              className="custom-modal-close"
+              onClick={() => setModalImage(null)}
+            >
+              ×
+            </button>
+            <img src={modalImage} alt="Enlarged project" />
+          </div>
+        </div>
+      )}
+
       <div
         className="mywork-showmore"
         onMouseEnter={() => setIsHovered(true)}
--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,5 +1,7 @@
-import clickfood1 from "./clickfood/clickfood.jpeg";
-import tictactoe1 from "./tictactoe/tictactoe.jpeg";
+import clickfood1 from "./clickfood/clickfood1.png";
+import clickfood2 from "../assets/clickfood/clickfood2.png";
+import tictactoe1 from "./tictactoe/tictactoe1.png";
+import tictactoe2 from "./tictactoe/tictactoe2.png";
 import smartbook1 from "./smartbook/smartbook.png";
 import portfolio1 from "./portfolio/portfolio.png";
 
@@ -7,13 +9,13 @@ const mywork_data = [
   {
     w_no: 1,
     w_name: "Click Food",
-    w_img: [clickfood1],
+    w_img: [clickfood1, clickfood2],
     github: "https://github.com/Riccardo-Giordanella/ClickFood",
   },
   {
     w_no: 2,
     w_name: "Tic Tac Toe",
-    w_img: [tictactoe1],
+    w_img: [tictactoe1, tictactoe2],
     github: "https://github.com/Riccardo-Giordanella/Tic-Tac-Toe",
   },
   {
